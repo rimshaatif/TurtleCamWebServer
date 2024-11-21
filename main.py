89ef9800a3670805ccab67374e19d75d87c47858
rimshaatif <rimshaatif@cmail.carleton.ca>
2024-11-20 19:35:54 -0500

    added status section

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, send_file
-
+import psutil
+import os
 app = Flask(__name__)
 
 @app.route('/upload', methods=['POST'])
@@ -15,11 +16,23 @@ def download_file():
     #return send_file(f'/path/to/files/{filename}', as_attachment=True)
     return render_template("download.html")
     
+def get_system_info():
+    info = {
+        
+        "memory": psutil.virtual_memory().percent,
+        "disk": psutil.disk_usage('/').percent,
+        "uptime": get_uptime()
+    }
+    return info
 
+def get_uptime():
+    # Get system uptime
+    return os.popen('uptime -p').read().strip()
 
 @app.route('/')
 def home():
-   return render_template('homepage.html')
+    status_info = get_system_info()
+    return render_template('homepage.html', status= status_info)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
